4aac66b1dfcf74944a6efcd2d0d2ebff9a85e731
elCubil <rodriguez.arias.miguel@gmail.com>
2026-03-25 14:51:47 -0500

    agregar_operacion_3

--- utils.h
+++ utils.h
@@ -5,4 +5,14 @@ int resta(int a,int b);
 
 int multiplicacion (int a,int b);
 
+
 int division (int a, int b);
+
+int division (int a, int b);
+
+int operacion_1(int a,int b);
+
+int operacion_2(int a,int b);
+
+int operacion_3(int a,int b);
+
